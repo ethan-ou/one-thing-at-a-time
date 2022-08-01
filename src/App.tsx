740c2b65ed37016869e864a40442f827389fa412
ethan-ou <ethantim@gmail.com>
2022-08-01 13:26:19 +1000

    Hacks

--- src/App.tsx
+++ src/App.tsx
@@ -29,13 +29,15 @@ function MainPage() {
           placeholder="Add Todos..."
         ></textarea>
       </div>
-      <button
-        className="px-4 py-2 bg-indigo-500 outline-none rounded text-white shadow-indigo-200 shadow-lg font-medium active:shadow-none active:scale-95 hover:bg-indigo-600 focus:bg-indigo-600 focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200"
-        onClick={onAdd}
-      >
-        Add +
-      </button>
-      <div>
+      <div className="flex justify-center pt-3">
+        <button
+          className="text-center px-4 py-2 bg-indigo-500 outline-none rounded text-white shadow-indigo-200 shadow-lg font-medium active:shadow-none active:scale-95 hover:bg-indigo-600 focus:bg-indigo-600 focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200"
+          onClick={onAdd}
+        >
+          Add +
+        </button>
+      </div>
+      <div className="text-center">
         <button onClick={() => setItems([])}>Clear All</button>
       </div>
       <MainList />
